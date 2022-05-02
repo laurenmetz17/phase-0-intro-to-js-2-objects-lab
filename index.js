aded02f47b72431d86a17fbbbf471d69ff9c0280
Lauren Metz <Laurenmetz15@gmail.com>
2022-05-01 20:48:06 -0400

    commit tree 54cac93e1607e12a8dc34a59ae8bb36b2ce6f028

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+const employee = {
+    name : "Lauren Metz",
+    adress : "1529 115th street"
+}
+
+//returns an employee with the original key value pairs and a new pair
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+//updates employee with a key and value destructively 
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+//deletes a key from an employee and returns the new employee
+function deleteFromEmployeeByKey(employee, key) {
+    const tempEmployee = {...employee};
+    delete tempEmployee[key];
+    return tempEmployee;
+}
+
+//destructively deletes a key from an employee
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+
+}
